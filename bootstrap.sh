08516a715381d847b5953a714afb6ee1478afd61
Darius Krištapavičius <darius@cuf.lt>
2014-08-25 00:32:22 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm;
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm;
 rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm;
 
-# config with enabled epel
+# config with enabled remi
 rm /etc/yum.repos.d/remi.repo;
 cp /bootstrap/etc/yum.repos.d/remi.repo /etc/yum.repos.d/remi.repo;
 
